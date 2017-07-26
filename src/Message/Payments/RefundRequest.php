5dc47966fe7ecf593a306c1819581ce77132a0ec
Yusnel Rojas Garcia <yrojass@gmail.com>
2017-07-26 10:59:18 -0400

    fix refund request

--- src/Message/Payments/RefundRequest.php
+++ src/Message/Payments/RefundRequest.php
@@ -11,7 +11,7 @@ use stdClass;
 class RefundRequest extends AbstractRequest
 {
     public function getSuccessStatus(){
-        return 'PendingSettlement';
+        return 'SettledSuccessfully';
     }
 
 	public function getData()
@@ -19,16 +19,45 @@ class RefundRequest extends AbstractRequest
         $this->validate('transactionReference');
 
 	    $request = $this->createRequest($this->getTransactionId());
-	    $ccAuthReversalService = new stdClass();
-	    $ccAuthReversalService->run = 'true';
-	    $ccAuthReversalService->authRequestID = $this->getTransactionReference();
+	    $ccCreditService = new stdClass();
+	    $ccCreditService->run = 'true';
+	    $ccCreditService->captureRequestID = $this->getTransactionReference();
 
-		$request->ccAuthReversalService = $ccAuthReversalService;
+        $request->ccAuthService = $ccCreditService;
+
+        $request->card = $this->buildCard(); //for stand-alone credits
+
+        if ($orderRequestToken = $this->getRequestToken()) {
+            $request->orderRequestToken = $orderRequestToken;
+        }
+
+        if ($items = $this->buildOrderItems()){
+            $request->item = $items;
+        }
+
+        if ($descriptor = $this->getDescription()){
+            $request->invoiceHeader = (object)array(
+                'merchantDescriptor'=>substr($descriptor, 0, 23)
+            );
+        }
+
+        $request->billTo = $this->buildBillingAddress();
+        $request->shipTo = $this->buildShippingAddress();
 
 	    $purchaseTotals = new stdClass();
 	    $purchaseTotals->grandTotalAmount = $this->getAmount();
 	    $request->purchaseTotals = $purchaseTotals;
 
         return $request;
+    }
+
+    public function getRequestToken()
+    {
+        return $this->getParameter('requestToken');
+    }
+
+    public function setRequestToken($value)
+    {
+        $this->setParameter('requestToken', $value);
     }
 }
